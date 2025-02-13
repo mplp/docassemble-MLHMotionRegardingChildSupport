02179832e9f94924e2263e5ea67397e3db44f0a7
Emily Miller <ekressmiller@gmail.com>
2025-02-13 13:56:59 -0500

    advance version

--- setup.py
+++ setup.py
@@ -44,9 +44,9 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.MLHMotionRegardingChildSupport',
-      version='1.0.2',
+      version='1.0.3',
       description=('A motion to change or get child support in Michigan'),
-      long_description='# docassemble.MLHMotionRegardingChildSupport\r\n\r\nA motion to change or get child support in Michigan\r\n\r\n## Authors:\r\n* Bryce Willey\r\n* Emily Kress Miller\r\n\r\n## Changelog:\r\n* 12/17/24  1.0.2 Update forms\r\n* 9/30/24   1.0.1 add user survey\r\n* 9/18/24   1.0.0 preparing to go live',
+      long_description='# docassemble.MLHMotionRegardingChildSupport\r\n\r\nA motion to change or get child support in Michigan\r\n\r\n## Authors:\r\n* Bryce Willey\r\n* Emily Kress Miller\r\n\r\n## Changelog:\r\n* 2/13/25   1.0.3 Behind-the-scenes court logic adjustment\r\n* 12/17/24  1.0.2 Update forms\r\n* 9/30/24   1.0.1 add user survey\r\n* 9/18/24   1.0.0 preparing to go live',
       long_description_content_type='text/markdown',
       author='Bryce Willey',
       author_email='ekressmiller@lsscm.org',
