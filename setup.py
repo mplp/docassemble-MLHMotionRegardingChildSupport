a1a21a9a6556a1706d3b907f36ff1798e73f8ab8
Emily Miller <ekressmiller@gmail.com>
2025-12-12 14:18:31 -0500

    update embed code

--- setup.py
+++ setup.py
@@ -44,17 +44,16 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.MLHMotionRegardingChildSupport',
-      version='1.0.4',
+      version='1.0.5',
       description=('A motion to change or get child support in Michigan'),
-      long_description='# docassemble.MLHMotionRegardingChildSupport\r\n\r\nA motion to change or get child support in Michigan\r\n\r\n## Authors:\r\n* Bryce Willey\r\n* Emily Kress Miller\r\n\r\n## Changelog:\r\n* 5/28/25   1.0.4 Update instructions header styling and language\r\n* 2/13/25   1.0.3 Behind-the-scenes court logic adjustment\r\n* 12/17/24  1.0.2 Update forms\r\n* 9/30/24   1.0.1 add user survey\r\n* 9/18/24   1.0.0 preparing to go live',
+      long_description='# docassemble.MLHMotionRegardingChildSupport\r\n\r\nA motion to change or get child support in Michigan\r\n\r\n## Authors:\r\n* Bryce Willey\r\n* Emily Kress Miller\r\n\r\n## Changelog:\r\n* 12/12/25  1.0.5 Update embeded survey\r\n* 5/28/25   1.0.4 Update instructions header styling and language\r\n* 2/13/25   1.0.3 Behind-the-scenes court logic adjustment\r\n* 12/17/24  1.0.2 Update forms\r\n* 9/30/24   1.0.1 add user survey\r\n* 9/18/24   1.0.0 preparing to go live',
       long_description_content_type='text/markdown',
       author='Bryce Willey',
       author_email='ekressmiller@lsscm.org',
-      license='The MIT License (MIT)',
+      license='MIT',
       url='https://michiganlegalhelp.org/resources/family/do-it-yourself-motion-change-or-get-child-support',
       packages=find_namespace_packages(),
       install_requires=[],
       zip_safe=False,
       package_data=find_package_data(where='docassemble/MLHMotionRegardingChildSupport/', package='docassemble.MLHMotionRegardingChildSupport'),
      )
-
